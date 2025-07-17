fb0bea848abc54d48fc9f4332dacccaa5cf930f2
Then Hope <115062829+QianyHP@users.noreply.github.com>
2025-07-17 21:08:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        if (this.getAttribute('href').startsWith('#')) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                const headerHeight = document.querySelector('.main-header').offsetHeight || 0;
+                window.scrollTo({
+                    top: targetElement.offsetTop - headerHeight - 20,
+                    behavior: 'smooth'
+                });
+            }
+        }
+    });
+});
